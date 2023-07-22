683c36f673e6b0e9d37eb01522cced07cf939a04
celery <71736183+celery6@users.noreply.github.com>
2023-07-21 21:52:46 -0400

    add sample config file

---
+++ configSample.ts
@@ -0,0 +1,8 @@
+const config = {
+    token: 'tokentokentokentokentokentokentokentokentokentoken',
+    clientId: 'idididididididididid',
+    mongoURI: 'mongodb://name:password@ip:ipPartTwo/databaseNAme',
+    test: true
+}
+
+export default config
